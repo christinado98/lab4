40f41c744b8ad4fe1b3d84bc0eea3913df22ce6a
Chau Do <c6do@ucsd.edu>
2020-01-29 23:24:09 -0800

    node.js lab

--- routes/hello.js
+++ routes/hello.js
@@ -1,10 +1,9 @@
-
 /*
  * GET home page.
  */
 
-exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+exports.view = function(req, res) {
+  res.render("index", {
+    name: ""
   });
 };
